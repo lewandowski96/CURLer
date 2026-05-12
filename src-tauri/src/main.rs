0230bae52ef13f454c594ad7b8014cba1455962e
Sanjeewa Nayanananda <ssanjeewa1996@gmail.com>
2026-05-12 12:24:04 +0530

    add header handling to backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,21 +1,38 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use serde::Deserialize;
 use std::process::Command;
+
+#[derive(Deserialize)]
+struct HttpHeader {
+    key: String,
+    value: String
+}
 
 // exposing the func to react frontent
 #[tauri::command]
 fn execute_curl(method: &str, url: &str) -> Result<String, String> {
-    // exec curl directly
+
+    let mut args = vec![
+        "-s".to_string(),
+        "-i".to_string(),
+        "-X".to_string(),
+        method.to_string(),
+    ];
+
+    for header in headers {
+        if !header.key.is_empty() {
+            args.push("-H".to_string());
+            args.push(format!("{}: {}", header.key, header.value));
+        }
+    }
+
+    args.push(url.to_string());
+
     // using array of arguments prevents command injections.
     let output = Command::new("curl")
-        .args([
-            "-s", // silent mode aka no progeress barr
-            "-i", // incl HTTP headers in the output
-            "-X",
-            method,
-            url
-        ])
+        .args(&args)
         .output()
         .map_err(|e| format!("Failed to execute curl: {}", e))?;
 
